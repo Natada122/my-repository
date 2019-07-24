e910379a961645874dea0d2dd569b0454a69a363
Natada122 <bynatada122@gmail.com>
2019-07-24 12:40:08 +0300

    Страница поиска и страница с результатами

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListOfResultsComponent } from "./list-of-results/list-of-results.component";
+import { SearchPageComponent } from "./search-page/search-page.component";
+import { FavoritesPageComponent } from "./favorites-page/favorites-page.component";
+import { PropertyListingPageComponent } from "./property-listing-page/property-listing-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: SearchPageComponent },
+  { path: "results", component: ListOfResultsComponent },
+  { path: "favorites", component: FavoritesPageComponent },
+  { path: "results/:id", component: PropertyListingPageComponent },
+  { path: "**", component: SearchPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListOfResultsComponent } from './list-of-results/list-of-results.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { PropertyListingPageComponent } from './property-listing-page/property-listing-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListOfResultsComponent, SearchPageComponent, FavoritesPageComponent, PropertyListingPageComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
